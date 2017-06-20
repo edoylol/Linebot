32d2613099c26b544167939d7226c8bcdb07f06e
Jessin Ra <Jessin Ra>
2017-06-20 16:34:27 +0700

    better

--- app.py
+++ app.py
@@ -269,8 +269,7 @@ class Function :
         jessin_userid = "U77035fb1a3a4a460be5631c408526d0b"
         try:
             try :
-                sender = line_bot_api.get_profile(event.source.group_id)
-                #sender = line_bot_api.get_profile(event.source.user_id).display_name
+                sender = line_bot_api.get_profile(event.source.user_id).display_name
             except :
                 sender = "Anonymous"
             report = Lines.report_note() % (original_text,sender)
