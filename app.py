de4d0d9dd8706d92945c195a45870a66e745f4b4
Jessin Ra <Jessin Ra>
2017-06-21 10:07:20 +0700

    test

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def get_receiver_addr(event):
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
-    global token, original_text, text, jessin_userid
+    global token, original_text, text, jessin_userid, tag_notifier_on
 
     jessin_userid = "U77035fb1a3a4a460be5631c408526d0b"
     original_text = event.message.text
