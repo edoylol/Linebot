1452b9ebe991fb9c2123f35d39c6af24acaab4b0
Jessin Ra <Jessin Ra>
2017-06-21 22:51:14 +0700

    ahaha

--- app.py
+++ app.py
@@ -473,15 +473,11 @@ class Function:
     def removed(event):
         user_id = event.source.user_id
         try:
-            user = line_bot_api.get_profile(event.source.user_id).display_name
+            user = line_bot_api.get_profile(user_id).display_name
 
         except:
             user = "someone"
 
-        reply = Lines.removed("removed")
-        line_bot_api.push_message(user_id, TextSendMessage(text=reply))
-        reply = Lines.removed("regards") % (user)
-        line_bot_api.push_message(user_id, TextSendMessage(text=reply))
         report = Lines.removed("report") % (user)
         line_bot_api.push_message(jessin_userid, TextSendMessage(text=report))
 
