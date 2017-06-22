fb64d4720af0c77bee5eaa91b8682c6f7264e545
Jessin Ra <Jessin Ra>
2017-06-23 00:03:25 +0700

    better

--- Database.py
+++ Database.py
@@ -1,6 +1,6 @@
 
 """ Special file dedicated for storing data """
-userlist = {}
+userlist = {'U77035fb1a3a4a460be5631c408526d0b': 'Jessin D', 'Ue93cccad22d1bff67371b9bdad100653': 'Shiori'}
 
 
 #backup userlist = {'U77035fb1a3a4a460be5631c408526d0b': 'Jessin D', 'Ue93cccad22d1bff67371b9bdad100653': 'Shiori'}
