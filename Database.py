47b0d740b5fa8dd8774f59ce3a26f19744ee622a
Jessin Ra <Jessin Ra>
2017-06-23 20:35:22 +0700

    han

--- Database.py
+++ Database.py
@@ -1,12 +1,12 @@
 
 """ Special file dedicated for storing data """
-userlist = {'U77035fb1a3a4a460be5631c408526d0b': 'Jessin D', 'Ue93cccad22d1bff67371b9bdad100653': 'Shiori'}
-aloy_goes_to_afaid = {}
+userlist = {'U77035fb1a3a4a460be5631c408526d0b': 'Jessin D', 'Ue93cccad22d1bff67371b9bdad100653': 'Shiori','U4bcb4dd4ae2cd44c03d4aa91b2f26a6c': 'Han'}
+aloy_goes_to_afaid = {'U4bcb4dd4ae2cd44c03d4aa91b2f26a6c': 'Han'}
 family = {'U77035fb1a3a4a460be5631c408526d0b': 'Jessin D', 'Ue93cccad22d1bff67371b9bdad100653': 'Shiori'}
 devlist = {'U77035fb1a3a4a460be5631c408526d0b': 'Jessin D'}
 
 list_dictionary = {"userlist":userlist,"aloy afaid" : aloy_goes_to_afaid,"family" : family,"dev" : devlist }
 
-#backup userlist = {'U77035fb1a3a4a460be5631c408526d0b': 'Jessin D', 'Ue93cccad22d1bff67371b9bdad100653': 'Shiori'}
+#backup userlist = {'U77035fb1a3a4a460be5631c408526d0b': 'Jessin D', 'Ue93cccad22d1bff67371b9bdad100653': 'Shiori','U4bcb4dd4ae2cd44c03d4aa91b2f26a6c': 'Han'}
 
 
