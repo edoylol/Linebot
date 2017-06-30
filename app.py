42b024e23508d27947940afa370caa6219d2dd89
Jessin Ra <Jessin Ra>
2017-06-30 19:45:00 +0700

    wiki

--- app.py
+++ app.py
@@ -871,218 +871,182 @@ class Function:
                 line_bot_api.push_message(address, TextSendMessage(text=report))
 
     def wiki_search():
-        try :
-            try :
-                def getting_page_title(mod_page):
-                    try:
-                        first_heading = mod_page.findAll("h1", {"id": "firstHeading"})
-                        page_title = first_heading[0].string
-                        return page_title
-                    except:
-                        return "page title doesn't exist"
+        def getting_page_title(mod_page):
+            try:
+                first_heading = mod_page.findAll("h1", {"id": "firstHeading"})
+                page_title = first_heading[0].string
+                return page_title
+            except:
+                return "page title doesn't exist"
 
-                def is_specific(mod_page):
-                    content = str(mod_page.find_all('p'))
-                    keyword = ["commonly refers to", "may also refer to", "may refer to"]
+        def is_specific(mod_page):
+            content = str(mod_page.find_all('p'))
+            keyword = ["commonly refers to", "may also refer to", "may refer to"]
 
-                    if any(word in content for word in keyword):
-                        return False
-                    else:
-                        return True
-
-                def has_disambiguation(mod_page):
-                    content = str(mod_page.find_all('a', {"class": "mw-disambig"}))
-                    keyword = ["disambiguation"]
-
-                    if any(word in content for word in keyword):
-                        return True
-                    else:
-                        return False
-
-            except LineBotApiError as e:
-                print("First 3 function error")
-                print(e.status_code)
-                print(e.error.message)
-                print(e.error.details)
-
-            try :
-                def first_paragraph_coordinate(mod_page):
-                    content = str(mod_page.find('p'))
-                    keyword = ["coordinate"]
-
-                    if any(word in content for word in keyword):
-                        return True
-                    else:
-                        return False
-
-                def get_paragraph(mod_page, cond='first'):
-
-                    def filter_paragraph_contents(content, start_sym='<', end_sym='>'):
-                        isfilter = (start_sym or end_sym) in content
-                        while isfilter:
-                            start_index = content.find(start_sym)
-                            stop_index = content.find(end_sym) + 1
-                            content = content.replace(str(content[start_index:stop_index]), "")
-                            isfilter = (start_sym or end_sym) in content
-                        return content
-
-                    if cond == 'first':
-                        content = mod_page.find('p')
-                    elif cond == 'second':
-                        content = mod_page.find_all('p')[1]
-                    content = filter_paragraph_contents(str(content), '<', '>')
-                    content = filter_paragraph_contents(str(content), '[', ']')
-                    return content
-
-                def show_suggestion(mod_page):
-                    suggestion = []
-                    keyword = ['All pages beginning with ', 'disambiguation', 'Categories', 'Disambiguation',
-                               'Place name disambiguation ', 'All article disambiguation ', 'All disambiguation ', 'Talk',
-                               'Contributions', 'Article', 'Talk', 'Read', 'Main page', 'Contents', 'Featured content',
-                               'Current events', 'Random article', 'Donate to Wikipedia', 'Help', 'About Wikipedia',
-                               'Community portal',
-                               'Recent changes', 'Contact page', 'What links here', 'Related changes', 'Upload file',
-                               'Special pages',
-                               'Wikidata item', 'Wikispecies', 'Cebuano', 'Čeština', 'Deutsch', 'Eesti', 'Español', 'Français',
-                               '한국어', 'Italiano', 'עברית', 'Latviešu', 'Magyar', 'Nederlands', '日本語', 'Norsk bokmål', 'پښتو',
-                               'Polski', 'Português', 'Русский', 'Simple English', 'Slovenščina', 'Српски / srpski',
-                               'Srpskohrvatski / српскохрватски', 'Suomi', 'Svenska', 'Türkçe', 'Українська', 'اردو', 'Volapük',
-                               'Edit links', 'Creative Commons Attribution-ShareAlike License', 'Terms of Use',
-                               'Privacy Policy',
-                               'Privacy policy', 'About Wikipedia', 'Disclaimers', 'Contact Wikipedia', 'Developers',
-                               'Cookie statement']
-
-                    links = mod_page.find_all('a')
-
-                    for link in links:
-                        href = str(link.get("href"))
-                        if "/wiki/" in (href[:6]):  # to eliminate wiktionary links
-                            if link.string is not None:
-                                if not (any(word in link.string for word in keyword)):
-                                    suggestion.append(link.string)
-                    suggestion = suggestion[:10]
-                    return suggestion
-
-            except LineBotApiError as e:
-                print("Middle 3 function error")
-                print(e.status_code)
-                print(e.error.message)
-                print(e.error.details)
-
-            try :
-                def get_search_keyword():
-
-                    split_text = OtherUtil.filter_words(text,"for wiki search")
-                    keyword = []
-                    for word in split_text:
-                        if "'" in word:
-                            keyword.append(word)
-
-                    if keyword == []:
-                        return
-                    else:
-                        keyword = " ".join(keyword)
-                        keyword = keyword.replace(" ", "_")
-                        return keyword
-
-                def get_search_language():
-
-                    try:
-                        split_text = OtherUtil.filter_words(text)
-                        index_now = 0
-                        for word in split_text:
-                            if word == "wiki":
-                                index_found = index_now - 1
-                                break
-                            index_now = index_now + 1
-                        language = split_text[index_found]
-
-                    except:
-                        language = 'en'  # default search language
-
-                    return language
-
-                def request_page():
-                    text = Lines.wiki_search("ask detail info")
-                    header_pic = Picture.header("background")
-                    buttons_template = ButtonsTemplate(text=text, thumbnail_image_url=header_pic, actions=[
-                        URITemplateAction(label=Labels.confirmation("yes"), uri=str(page_url))
-                    ])
-                    template_message = TemplateSendMessage(alt_text=text, template=buttons_template)
-                    line_bot_api.push_message(address, template_message)
-
-            except LineBotApiError as e:
-                print("Last 3 function error")
-                print(e.status_code)
-                print(e.error.message)
-                print(e.error.details)
-
-            keyword = get_search_keyword()
-            language = get_search_language()
-            report = []
-            request_detailed_info = False
-
-            if keyword != None:
-                try:
-                    keyword = keyword[1:-1]
-                    page_url = "https://" + language + ".wikipedia.org/wiki/" + keyword
-                    try :
-                        req = urllib.request.Request(page_url, headers={'User-Agent': "Magic Browser"})
-                        con = urllib.request.urlopen(req)
-                        page_source_code_text = con.read()
-                        mod_page = BeautifulSoup(page_source_code_text, "html.parser")
-                    except :
-                        print("YEAHAHHHHH")
-
-                    exist = True
-                except LineBotApiError as e:
-                    print("PAGE NOT FOUND ERROR")
-                    print(e.status_code)
-                    print(e.error.message)
-                    print(e.error.details)
-                    report.append(Lines.wiki_search("page not found") % (language,keyword))
-                    exist = False
-
-                try :
-                    if exist:
-                        title = getting_page_title(mod_page)
-                        if is_specific(mod_page):
-                            report.append(title)
-                            if has_disambiguation(mod_page):
-                                report.append(Lines.wiki_search("has disambiguation"))
-
-                            if first_paragraph_coordinate(mod_page) :
-                                report.append(get_paragraph(mod_page, 'second'))
-                            else:
-                                report.append(get_paragraph(mod_page, 'first'))
-
-                            request_detailed_info = True
-
-                        else:
-                            report.append(Lines.wiki_search("not specific page - header") % keyword)
-                            report.append(Lines.wiki_search("not specific page - content") % keyword)
-                            suggestion = show_suggestion(mod_page)
-                            suggestion = "\n".join(suggestion)
-                            report.append(suggestion)
-
-                except LineBotApiError as e:
-                    print("IF EXIST ERROR")
-                    print(e.status_code)
-                    print(e.error.message)
-                    print(e.error.details)
+            if any(word in content for word in keyword):
+                return False
             else:
-                report.append(Lines.wiki_search("no keyword found"))
+                return True
 
-            report = "\n".join(report)
-            line_bot_api.push_message(address, TextSendMessage(text=report))
-            if request_detailed_info :
-                request_page()
+        def has_disambiguation(mod_page):
+            content = str(mod_page.find_all('a', {"class": "mw-disambig"}))
+            keyword = ["disambiguation"]
 
-        except LineBotApiError as e:
-            print("Major function error")
-            print(e.status_code)
-            print(e.error.message)
-            print(e.error.details)
+            if any(word in content for word in keyword):
+                return True
+            else:
+                return False
+
+        def first_paragraph_coordinate(mod_page):
+            content = str(mod_page.find('p'))
+            keyword = ["coordinate"]
+
+            if any(word in content for word in keyword):
+                return True
+            else:
+                return False
+
+        def get_paragraph(mod_page, cond='first'):
+
+            def filter_paragraph_contents(content, start_sym='<', end_sym='>'):
+                isfilter = (start_sym or end_sym) in content
+                while isfilter:
+                    start_index = content.find(start_sym)
+                    stop_index = content.find(end_sym) + 1
+                    content = content.replace(str(content[start_index:stop_index]), "")
+                    isfilter = (start_sym or end_sym) in content
+                return content
+
+            if cond == 'first':
+                content = mod_page.find('p')
+            elif cond == 'second':
+                content = mod_page.find_all('p')[1]
+            content = filter_paragraph_contents(str(content), '<', '>')
+            content = filter_paragraph_contents(str(content), '[', ']')
+            return content
+
+        def show_suggestion(mod_page):
+            suggestion = []
+            keyword = ['All pages beginning with ', 'disambiguation', 'Categories', 'Disambiguation',
+                       'Place name disambiguation ', 'All article disambiguation ', 'All disambiguation ', 'Talk',
+                       'Contributions', 'Article', 'Talk', 'Read', 'Main page', 'Contents', 'Featured content',
+                       'Current events', 'Random article', 'Donate to Wikipedia', 'Help', 'About Wikipedia',
+                       'Community portal',
+                       'Recent changes', 'Contact page', 'What links here', 'Related changes', 'Upload file',
+                       'Special pages',
+                       'Wikidata item', 'Wikispecies', 'Cebuano', 'Čeština', 'Deutsch', 'Eesti', 'Español',
+                       'Français',
+                       '한국어', 'Italiano', 'עברית', 'Latviešu', 'Magyar', 'Nederlands', '日本語', 'Norsk bokmål',
+                       'پښتو',
+                       'Polski', 'Português', 'Русский', 'Simple English', 'Slovenščina', 'Српски / srpski',
+                       'Srpskohrvatski / српскохрватски', 'Suomi', 'Svenska', 'Türkçe', 'Українська', 'اردو',
+                       'Volapük',
+                       'Edit links', 'Creative Commons Attribution-ShareAlike License', 'Terms of Use',
+                       'Privacy Policy',
+                       'Privacy policy', 'About Wikipedia', 'Disclaimers', 'Contact Wikipedia', 'Developers',
+                       'Cookie statement']
+
+            links = mod_page.find_all('a')
+
+            for link in links:
+                href = str(link.get("href"))
+                if "/wiki/" in (href[:6]):  # to eliminate wiktionary links
+                    if link.string is not None:
+                        if not (any(word in link.string for word in keyword)):
+                            suggestion.append(link.string)
+            suggestion = suggestion[:10]
+            return suggestion
+
+        def get_search_keyword():
+
+            split_text = OtherUtil.filter_words(text, "for wiki search")
+            keyword = []
+            for word in split_text:
+                if "'" in word:
+                    keyword.append(word)
+
+            if keyword == []:
+                return
+            else:
+                keyword = " ".join(keyword)
+                keyword = keyword.replace(" ", "_")
+                return keyword
+
+        def get_search_language():
+
+            try:
+                split_text = OtherUtil.filter_words(text)
+                index_now = 0
+                for word in split_text:
+                    if word == "wiki":
+                        index_found = index_now - 1
+                        break
+                    index_now = index_now + 1
+                language = split_text[index_found]
+
+            except:
+                language = 'en'  # default search language
+
+            return language
+
+        def request_page():
+            text = Lines.wiki_search("ask detail info")
+            header_pic = Picture.header("background")
+            buttons_template = ButtonsTemplate(text=text, thumbnail_image_url=header_pic, actions=[
+                URITemplateAction(label=Labels.confirmation("yes"), uri=str(page_url))
+            ])
+            template_message = TemplateSendMessage(alt_text=text, template=buttons_template)
+            line_bot_api.push_message(address, template_message)
+
+        keyword = get_search_keyword()
+        language = get_search_language()
+        report = []
+        request_detailed_info = False
+
+        if keyword != None:
+            try:
+                keyword = keyword[1:-1]
+                page_url = "https://" + language + ".wikipedia.org/wiki/" + keyword
+                req = urllib.request.Request(page_url, headers={'User-Agent': "Magic Browser"})
+                con = urllib.request.urlopen(req)
+                page_source_code_text = con.read()
+                mod_page = BeautifulSoup(page_source_code_text, "html.parser")
+                exist = True
+            except LineBotApiError as e:
+                print("PAGE NOT FOUND ERROR")
+                print(e.status_code)
+                print(e.error.message)
+                print(e.error.details)
+                report.append(Lines.wiki_search("page not found") % (language, keyword))
+                exist = False
+
+            if exist:
+                title = getting_page_title(mod_page)
+                if is_specific(mod_page):
+                    report.append(title)
+                    if has_disambiguation(mod_page):
+                        report.append(Lines.wiki_search("has disambiguation"))
+
+                    if first_paragraph_coordinate(mod_page):
+                        report.append(get_paragraph(mod_page, 'second'))
+                    else:
+                        report.append(get_paragraph(mod_page, 'first'))
+
+                    request_detailed_info = True
+
+                else:
+                    report.append(Lines.wiki_search("not specific page - header") % keyword)
+                    report.append(Lines.wiki_search("not specific page - content") % keyword)
+                    suggestion = show_suggestion(mod_page)
+                    suggestion = "\n".join(suggestion)
+                    report.append(suggestion)
+        else:
+            report.append(Lines.wiki_search("no keyword found"))
+
+        report = "\n".join(report)
+        line_bot_api.push_message(address, TextSendMessage(text=report))
+        if request_detailed_info:
+            request_page()
 
     """====================== Sub Function List ============================="""
 
