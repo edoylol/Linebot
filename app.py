bfd8a73a2c3162af15a2bd8302e6fe3f88a9a5d4
JessinRa <jessin.bdg@gmail.com>
2017-08-08 13:28:10 +0700

    better

--- app.py
+++ app.py
@@ -3332,6 +3332,7 @@ class Function:
         def send_detail_result(filtered_video_link, filtered_video_description):
             """ Function to send carousel of direct download link """
 
+            # Generate carousel
             carousel_text = filtered_video_description
             columns = []
             for i in range(0, len(filtered_video_link)):
@@ -3339,18 +3340,8 @@ class Function:
                 columns.append(carousel_column)
 
             carousel_template = CarouselTemplate(columns=columns)
-            template_message = TemplateSendMessage(alt_text="Playing music..", template=carousel_template)
+            template_message = TemplateSendMessage(alt_text="Playing music...", template=carousel_template)
             line_bot_api.push_message(address, template_message)
-
-
-            buttons_template = ButtonsTemplate(text="test", thumbnail_image_url="https://i.ytimg.com/vi/4bmUFRxNEIg/maxresdefault.jpg", actions=[
-                PostbackTemplateAction(label='Count me in', data=''),
-                PostbackTemplateAction(label='No thanks', data=''),
-                PostbackTemplateAction(label='Decide later', data='')
-            ])
-            template_message = TemplateSendMessage(alt_text="test", template=buttons_template)
-            line_bot_api.push_message(address, template_message)
-
 
             # Send footer to end the request
             if len(filtered_video_link) > 1:
